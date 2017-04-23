3ca5d75db2863d2cc377c746b0f3485d9e8d73df
Martin <sickan843@live.se>
2017-04-23 20:56:28 +0200

    Testing

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,14 @@
-FROM alpine:3.1
+FROM node:latest
 
-# Update
-RUN apk add --update nodejs
-RUN npm -g install npm@latest
+# Create app directory
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
 
 # Install app dependencies
-COPY package.json /src/package.json
-RUN cd /src; npm install
+COPY package.json /usr/src/app/
+RUN npm install
 
 # Bundle app source
-COPY . /src
+COPY . /usr/src/app
 
-EXPOSE  8080
 CMD ["node", "/src/index.js"]
--- index.js
+++ index.js
@@ -5,11 +5,11 @@ client.connect({
     token: 'MzA1NjQxNzg0MzYzNDUwMzkw.C94hJA.wBtsO2sF2rt5ZcIDRRLUCTuwMJg'
 })
 
-client.Dispatcher.on(Discordie.Events.GATEWAY_READY, e => {
+client.Dispatcher.on(Discordie.Events.GATEWAY_READY, function(e) {
     console.log(`Connected as: ${client.User.username}`)
 })
 
-client.Dispatcher.on(Discordie.Events.MESSAGE_CREATE, e => {
+client.Dispatcher.on(Discordie.Events.MESSAGE_CREATE, function(e) {
     if (e.message.content === 'PING')
         e.message.channel.sendMessage('PONG')
     console.log(e)
